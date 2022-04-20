2529ca5f9f242a3858bdadf6cb5dff0cb4773225
Chris Godwin <patchshorts@gmail.com>
2022-04-20 14:04:52 -0500

    test ci change

--- bin/get-pce.sh
+++ bin/get-pce.sh
@@ -5,4 +5,3 @@ if [ "${PRUNE}" = "" ];then  PRUNE=1;fi
 if [ $PRUNE -gt 0 ];then  rm -rf openordu-pce;rm -rf ./src/public-celtic-encyclopedia;fi
 git clone --branch=$BRANCH git@github.com:openordu/pce.git openordu-pce
 mv openordu-pce/src ./src/public-celtic-encyclopedia && rm -rf openordu-pce
-yarn docs:dev
