fcf023869d5d87a4f7f5604d351b8d32a26890c5
Chris Godwin <patchshorts@gmail.com>
2022-04-22 23:08:18 -0500

    more zh removal

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -20,11 +20,6 @@ export default defineHopeConfig({
       title: "Theme Demo",
       description: "A demo for vuepress-theme-hope",
     },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "主题演示",
-      description: "vuepress-theme-hope 的演示",
-    },
   },
 
   themeConfig,
