b127f2be2dd03dedf7c672f9103f446b73258eb5
Chris Godwin <patchshorts@gmail.com>
2022-06-04 22:26:15 -0500

    updating submodule commits

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 cd `dirname $0`
-git submodule update --remote --recursive --force
+git submodule update --init --remote --recursive --force
 git submodule
 git add .
 git commit -m 'updating submodule commits'
